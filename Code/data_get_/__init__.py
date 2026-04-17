7c4eaa967bff55b7bc4787d19c8a49146ac09efe
wen_hhh <93653103+ONE-WMZ@users.noreply.github.com>
2026-04-17 18:11:52 +0800

    Update __init__.py

--- Code/data_get_/__init__.py
+++ Code/data_get_/__init__.py
@@ -8,12 +8,12 @@ from generate_ import users_list, normal_list
 
 
 selected_channel = [
-    'F7-T7', 'T7-P7',         # 左颞叶
-    'F8-T8', 'T8-P8-0',       # 右颞叶
-    'T7-FT9', 'FT10-T8',      # 颞叶深部
-    'FP1-F3', 'FP2-F4',       # 前额叶
-    'F3-C3', 'F4-C4',         # 额中央区
-    'C3-P3', 'C4-P4',         # 中央至顶叶
-    'FZ-CZ', 'CZ-PZ',         # 中线结构
-    'P7-O1', 'P8-O2'          # 枕叶
+    'F7-T7', 'T7-P7',      
+    'F8-T8', 'T8-P8-0',     
+    'T7-FT9', 'FT10-T8',      
+    'FP1-F3', 'FP2-F4',    
+    'F3-C3', 'F4-C4',      
+    'C3-P3', 'C4-P4',       
+    'FZ-CZ', 'CZ-PZ',        
+    'P7-O1', 'P8-O2'         
 ]
